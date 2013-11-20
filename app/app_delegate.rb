a3ba13b9b5ed342df16e2c118e6b376f12eb7312
Andrew Babichev <andrew.babichev@gmail.com>
2013-11-20 22:15:19 +0200

    Init

---
+++ app/app_delegate.rb
@@ -0,0 +1,14 @@
+class AppDelegate
+  def applicationDidFinishLaunching(notification)
+    buildMenu
+    buildWindow
+  end
+
+  def buildWindow
+    @main_window_controller = MainWindowController.new
+    @main_window = @main_window_controller.window
+
+    @main_window.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
+    @main_window.orderFrontRegardless
+  end
+end
