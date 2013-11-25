e8167443bef6983fb428859567bf4568e84f763e
Andrew Babichev <andrew.babichev@gmail.com>
2013-11-26 00:07:22 +0200

    Messages controller init

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,12 @@
 class MessagesController < NSViewController
 
-  layout do |root|
-    subview WebView,
-      frame: root.bounds,
-      autoresizingMask: NSViewWidthSizable | NSViewHeightSizable
+  def loadView
+    super
+    layout self.view, :messages_controller do
+      @message_view = MessageView.new
+      @message_view.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString("http://google.com")))
+      subview @message_view, :message_view
+    end
   end
 
 end
