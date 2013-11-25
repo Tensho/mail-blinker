5807d5e3cd184acedbd6745d7a260e5ee607d1ab
Andrew Babichev <andrew.babichev@gmail.com>
2013-11-26 00:08:35 +0200

    Main style init

---
+++ app/styles/main.rb
@@ -0,0 +1,10 @@
+Teacup::Stylesheet.new :main do
+  # style :main_window,
+  #   frame: [[10, 10], [1000, 1000]]
+
+  style :messages_controller,
+    frame: [[10, 10], [500, 500]]
+
+  style :message_view,
+    frame: [[10, 10], [300, 300]]
+end
