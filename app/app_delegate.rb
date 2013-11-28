fb602b920c0880ad3c9279baf66bd302710a6fcf
Andrew Babichev <andrew.babichev@gmail.com>
2013-11-28 22:51:39 +0200

    Center window

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -7,8 +7,8 @@ class AppDelegate
   def buildWindow
     @main_window_controller = MainWindowController.new
     @main_window = @main_window_controller.window
-
     @main_window.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
+    @main_window.center
     @main_window.orderFrontRegardless
   end
 end
