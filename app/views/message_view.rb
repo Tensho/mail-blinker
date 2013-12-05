a3e3874d7688af5f06434165ec98156affeaa072
Andrew Babichev <andrew.babichev@gmail.com>
2013-12-05 21:53:08 +0200

    Message view look preferences

--- app/views/message_view.rb
+++ app/views/message_view.rb
@@ -4,6 +4,7 @@ class MessageView < WebView
       self.mainFrame.loadRequest(NSURLRequest.requestWithURL(NSURL.URLWithString("http://habrahabr.ru")))
 
       look_webview self
+      look_preferences self.preferences
     end
   end
 
@@ -24,5 +25,28 @@ class MessageView < WebView
     puts "UIDelegate: #{webview.UIDelegate}"
     puts "hostWindow: #{webview.hostWindow}"
     puts "estimatedProgress: #{webview.estimatedProgress}"
+  end
+
+  def look_preferences preferences
+    puts "=" * 80
+    puts "Preferences #{preferences}"
+    puts "=" * 80
+    puts "autosaves: #{preferences.autosaves}"
+    puts "isJavaEnabled: #{preferences.isJavaEnabled}"
+    puts "isJavaScriptEnabled: #{preferences.isJavaScriptEnabled}"
+    puts "arePlugInsEnabled: #{preferences.arePlugInsEnabled}"
+    puts "userStyleSheetEnabled: #{preferences.userStyleSheetEnabled}"
+    puts "userStyleSheetLocation: #{preferences.userStyleSheetLocation}"
+    puts "cursiveFontFamily: #{preferences.cursiveFontFamily}"
+    puts "sansSerifFontFamily: #{preferences.sansSerifFontFamily}"
+    puts "standardFontFamily: #{preferences.standardFontFamily}"
+    puts "defaultFixedFontSize: #{preferences.defaultFixedFontSize}"
+    puts "defaultFontSize: #{preferences.defaultFontSize}"
+    puts "defaultTextEncodingName: #{preferences.defaultTextEncodingName}"
+    puts "loadsImagesAutomatically: #{preferences.loadsImagesAutomatically}"
+    puts "privateBrowsingEnabled: #{preferences.privateBrowsingEnabled}"
+    puts "tabsToLinks: #{preferences.tabsToLinks}"
+    puts "usesPageCache: #{preferences.usesPageCache}"
+    puts "cacheModel: #{preferences.cacheModel}"
   end
 end
