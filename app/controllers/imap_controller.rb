1c63d4050c54a1907d037a5006627e995c462e4e
Andrew Babichev <andrew.babichev@gmail.com>
2014-01-19 21:45:39 +0200

    IMAP controller MailCore HTML rendering variants

--- app/controllers/imap_controller.rb
+++ app/controllers/imap_controller.rb
@@ -1,5 +1,8 @@
 class IMAPController
 
+  REQUEST_KIND = MCOIMAPMessagesRequestKindHeaders | MCOIMAPMessagesRequestKindStructure
+  UIDS = MCOIndexSet.indexSetWithRange MCORangeMake(1, UINT64_MAX)
+
   def initialize
     @session = MCOIMAPSession.new
     @session.hostname = 'imap.gmail.com'
@@ -14,21 +17,58 @@ class IMAPController
     @session.checkAccountOperation.
     start(-> (error) do
       if error
-        puts error.localizedDescription.red
+        puts error.localizedDescription
       else
-        requestKind = MCOIMAPMessagesRequestKindHeaders
-        folder = "INBOX"
-        uids = MCOIndexSet.indexSetWithRange MCORangeMake(1, UINT64_MAX)
-
-        @session.fetchMessagesByUIDOperationWithFolder(folder, requestKind:requestKind, uids:uids).
-        start(-> (error, messages, vanished_messages) do
+        @session.fetchNamespaceOperation.start(-> (error, namespaces) do
           if error
-            puts error.localizedDescription.red
+            puts error.localizedDescription
           else
-            puts "#{messages.count} messages"
-            messages.each do |message|
-              puts message.header.subject
-            end
+            @session.setDefaultNamespace(namespaces[MCOIMAPNamespacePersonal])
+
+            @session.fetchAllFoldersOperation.
+            start(-> (error, folders) do
+              folders.each do |folder|
+                folder.local_path = @session.defaultNamespace.componentsFromPath(folder.path).join('/')
+
+                App.delegate.build_folder_window(folder)
+
+                @session.fetchMessagesByUIDOperationWithFolder(folder.path, requestKind: REQUEST_KIND, uids: UIDS).
+                start(-> (error, imap_messages, vanished_messages) do
+                  if error
+                    puts error.localizedDescription
+                  else
+                    puts "#{folder.local_path}: #{imap_messages.count} messages"
+
+                    imap_messages.each do |imap_message|
+                      puts imap_messages.map(&:header).map(&:subject)
+                      # imap_message = imap_messages.last
+                      # puts imap_message.header.subject
+
+                      # MCOIMAPSession#htmlRenderingOperationWithMessage
+
+                      @session.htmlRenderingOperationWithMessage(imap_message, folder: folder.path).
+                      start(-> (html, error) do
+                        App.notification_center.post :reload_notification, nil, html: html
+                      end)
+
+                      # MCOIMAPMessage#htmlRenderingWithFolder
+
+                      # html = imap_message.htmlRenderingWithFolder folder, delegate: MessageViewMCOHTMLRendererIMAPDelegate
+                      # App.notification_center.post :reload_notification, nil, html: html
+
+                      # MCOIMAPMessage#fetchMessageByUIDOperationWithFolder
+
+                      # @session.fetchMessageByUIDOperationWithFolder(folder, uid: imap_message.uid).
+                      # start(-> (error, message_data) do
+                      #   message_parser = MCOMessageParser.messageParserWithData message_data
+                      #   html = message_parser.htmlRenderingWithDelegate MessageViewMCOHTMLRendererDelegate
+                      #   App.notification_center.post :reload_notification, nil, html: html
+                      # end)
+                    end
+                  end
+                end)
+              end
+            end)
           end
         end)
       end
